5f27c4c45d7257e6822a513919b260ce52ebfe05
Backins <backinsmail@gmail.com>
2017-10-19 22:21:41 +0200

    Modification du index.php - Retrait d'un EXPOSE

--- applications/index.php
+++ applications/index.php
@@ -1,8 +1,19 @@
 <?php
 
-echo "Well done ! Installation réussie";
+echo "Well done ! Installation réussie"; 
+$etatConnexion = "<br>Connexion à la base de données réussie !";
+if (ereg("Win", getenv("HTTP_USER_AGENT"))){
+	$link = mysql_connect("192.168.99.100:33061", "database", "root") or die("Impossible de se connecter : " . mysql_error());
+}
+elseif ((ereg("Mac", getenv("HTTP_USER_AGENT"))) || (ereg("PPC", getenv("HTTP_USER_AGENT")))){
+	$link = mysql_connect("localhost:33061", "database", "root") or die("Impossible de se connecter : " . mysql_error());
+}
+elseif (ereg("Linux", getenv("HTTP_USER_AGENT"))) {
+	$link = mysql_connect("localhost:33061", "database", "root") or die("Impossible de se connecter : " . mysql_error());
+} else {
+	$etatConnexion = "";
+}
 
-$link = mysql_connect("192.168.99.100:33061", "database", "root") or die("Impossible de se connecter : " . mysql_error());
+echo $etatConnexion;
 
-echo '<br>Connexion à la base de données réussie !';
 mysql_close($link);
