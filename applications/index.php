4bd7f3ce3467d52498bec5767b0e2a6ac6dbc68c
Backins <backinsmail@gmail.com>
2017-10-19 20:23:39 +0200

    Ajout de la connexion a la BDD sur index.php

--- applications/index.php
+++ applications/index.php
@@ -1,3 +1,8 @@
 <?php
 
 echo "Well done ! Installation réussie";
+
+$link = mysql_connect("192.168.99.100:33061", "database", "root") or die("Impossible de se connecter : " . mysql_error());
+
+echo '<br>Connexion à la base de données réussie !';
+mysql_close($link);
