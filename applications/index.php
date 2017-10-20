57c35163122e6ae99202a7b5e9acd7fc3eb12abb
Julien Domange <domangejulien@gmail.com>
2017-10-20 09:37:25 +0200

    Solution trouvée pour la connexion BDD sur index.php

--- applications/index.php
+++ applications/index.php
@@ -6,10 +6,10 @@ if (ereg("Win", getenv("HTTP_USER_AGENT"))){
 	$link = mysql_connect("192.168.99.100:33061", "database", "root") or die("Impossible de se connecter : " . mysql_error());
 }
 elseif ((ereg("Mac", getenv("HTTP_USER_AGENT"))) || (ereg("PPC", getenv("HTTP_USER_AGENT")))){
-	$link = mysql_connect("localhost:33061", "database", "root") or die("Impossible de se connecter : " . mysql_error());
+	$link = mysql_connect("build_database_1", "database", "root") or die("Impossible de se connecter : " . mysql_error());
 }
 elseif (ereg("Linux", getenv("HTTP_USER_AGENT"))) {
-	$link = mysql_connect("localhost:33061", "database", "root") or die("Impossible de se connecter : " . mysql_error());
+	$link = mysql_connect("build_database_1", "database", "root") or die("Impossible de se connecter : " . mysql_error());
 } else {
 	$etatConnexion = "";
 }
